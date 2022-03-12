f1acc96578c0a9a8db178b02cbb0bffa1013f194
barreraIud <alberto.barrera@est.iudigital.edu.co>
2022-03-12 02:16:10 -0500

    se cambiaron credenciales de conexion

--- conexion.php
+++ conexion.php
@@ -6,19 +6,7 @@ class BD{
     public static function crearInstancia(){
         if(!isset( self::$instancia)){ //si no hay ninguna conexion
             $opcionesPDO[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;//cambiar el acceso de la conexion
-            //self::$instancia= new PDO('mysql:host=us-cdbr-east-05.cleardb.net;dbname=heroku_17167557b982c9b','b22e70d97e3eb4','9b51788f', $opcionesPDO); 
-            //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
-            
+            self::$instancia= new PDO('mysql:host=us-cdbr-east-05.cleardb.net;dbname=heroku_17167557b982c9b','b22e70d97e3eb4','9b51788f', $opcionesPDO);             
             //echo "conexiòn realizada";
         }
         return self::$instancia;//si ya hay una conexion la retornamos y si no hay la creamos
