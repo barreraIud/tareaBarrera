8a941869e97d639d451846fd640cd05a18d46a77
barreraIud <alberto.barrera@est.iudigital.edu.co>
2022-03-12 01:33:56 -0500

    se cambiaron credenciales de conexion

--- conexion.php
+++ conexion.php
@@ -6,7 +6,7 @@ class BD{
     public static function crearInstancia(){
         if(!isset( self::$instancia)){ //si no hay ninguna conexion
             $opcionesPDO[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;//cambiar el acceso de la conexion
-            self::$instancia= new PDO('mysql:host=us-cdbr-east-05.cleardb.net;dbname=heroku_17167557b982c9b','b22e70d97e3eb4','9b51788f', $opcionesPDO); 
+            self::$instancia= new PDO('mysql:host=us-cdbr-east-05.cleardb.net;dbname=biblioteca','b22e70d97e3eb4','9b51788f', $opcionesPDO); 
             //echo "conexiòn realizada";
         }
         return self::$instancia;//si ya hay una conexion la retornamos y si no hay la creamos
